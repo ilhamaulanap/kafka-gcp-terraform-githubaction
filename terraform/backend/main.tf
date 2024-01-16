1b4e47388c2825717f73006c20309ab319733ee4
Ilham Maulana Putra <52365731+ilhamaulanap@users.noreply.github.com>
2024-01-17 01:32:34 +0700

    fix workflows

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -3,10 +3,17 @@ provider "google" {
   region      = var.gcp_region
 }
 
-## bucker for terraform state
+##check bucket for terraform state
+
+data "google_storage_bucket" "existing_bucket" {
+  name = var.gcs_bucket_terraform
+}
+
 
 resource "google_storage_bucket" "terraform_backend" {
+  count         = data.google_storage_bucket.existing_bucket ? 0 : 1
   name          = var.gcs_bucket_terraform
   location      = var.gcs_bucket_location
   force_destroy = true
 }
+
