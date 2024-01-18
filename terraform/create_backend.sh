ca01c54264ddb5b28daff776db8a672ca70335e2
Ilham Maulana Putra <52365731+ilhamaulanap@users.noreply.github.com>
2024-01-18 15:33:05 +0700

    create_backend.sh to workflow

--- terraform/create_backend.sh
+++ terraform/create_backend.sh
@@ -1,7 +1,7 @@
 backend_bucket=$1
 gcs_bucket_location=$2
 
-#check if the bucket already exists, if not create new bucket
+#check if the bucket already exists, if not create new 
 if ! gsutil ls -b gs://${backend_bucket}; then
     gsutil mb -l ${gcs_bucket_location} gs://${backend_bucket}
     echo "Bucket created successfully!"
