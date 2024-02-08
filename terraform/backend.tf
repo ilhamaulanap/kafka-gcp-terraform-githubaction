66b74b2148494b322228cb4081cbdc9f2131dc8c
Ilham Maulana Putra <52365731+ilhamaulanap@users.noreply.github.com>
2024-02-08 11:44:22 +0700

    change backend bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 #create backend-bucket to stroe terraform state files
 terraform {
   backend "gcs" {
-    bucket         = "kafka-terraform-state-backend"
+    bucket         = "kafka-terraform-state-backend-202402"
     prefix         = "/terraform.tfstate"
   }
 }
